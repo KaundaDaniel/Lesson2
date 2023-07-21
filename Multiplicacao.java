079324b75d464c327e5eab232c6f56129640ed59
KaundaDaniel <dannydagmaisutic@gmail.com>
2023-07-21 22:21:36 +0100

    Multiplicação de numero

---
+++ Multiplicacao.java
@@ -0,0 +1,8 @@
+public class Multiplicacao {
+    public static void main(String[] args) {
+        System.out.println("Multiplicação de Nùmeros");
+        int n1=89,n2=34;
+        int multiplica=n1*n2;
+        System.out.println("Resultado: "+multiplica);
+    }
+}
